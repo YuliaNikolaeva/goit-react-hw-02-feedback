32c1ca2bb5b4cd7e60d326fc0936979e8bc8203a
Yulia Nikolaeva <y.nikolaeva@rndev.org>
2020-06-24 15:53:29 +0300

    Updates

---
+++ precache-manifest.9288f5f17347e3ce609fd7a057065a9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6ba58222019393999f3f29a3a44a76c",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "f23e5b380d92ce532604",
+    "url": "/goit-react-hw-02-feedback/static/css/main.05fdc818.chunk.css"
+  },
+  {
+    "revision": "772777064e0e38e61a11",
+    "url": "/goit-react-hw-02-feedback/static/js/2.42f413f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.42f413f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f23e5b380d92ce532604",
+    "url": "/goit-react-hw-02-feedback/static/js/main.06c7f7fd.chunk.js"
+  },
+  {
+    "revision": "7da700a07c17c956736d",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.0122c205.js"
+  }
+]);
